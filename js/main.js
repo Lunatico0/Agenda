373f2e9654f3fec5116a08710d549f3d75d0bd0c
Patricio Pittana <pittanapatricio@gmail.com>
2024-03-13 20:56:49 -0300

    Agenda personal con posibilidad de agregar, editar y mostrar contactos, falta eliminar contactos

--- js/main.js
+++ js/main.js
@@ -5,19 +5,18 @@ do{
     option = prompt("📙Bienvenido a la Agenda📙.\n\n1-➕Ingresar nuevo contacto➕.\n2-🖋️Editar contacto🖋️.\n3-🚫Eliminar contacto🚫.\n4-📓Mostrar lista de contactos📓.\n5-Salir.");
     switch (option){
         case "1":
-            // Llama a la función para comenzar a agregar contactos
-            agregarContacto();        
+            agregarContacto();                                                      // Llama a la funcion para comenzar a agregar contactos
             break;
         case "2":
+            editarContacto();
             break;
         case "3":
             break;
         case "4":
-            // Llama a la función para comenzar a mostrar contactos
-            mostrarContactos();
+            mostrarContactos();                                                     // Llama a la funcion para mostrar contactos
             break;
         case "salir", "5":
-            alert("Gracias, vuelva pronto.")
+            alert("Gracias, vuelva pronto.")                                        // Termina el programa
             break;
         default:
             alert("Opcion incorrecta. Gracias, vuelva pronto.");
@@ -26,49 +25,7 @@ do{
 } while (option.toLowerCase() !== "salir" && option !== "5");
 
 
-// for (let i = 0; i < 3; i++) {
-//     const nombre = prompt("Ingrese el nombre del contacto");
-//     const apellido = prompt("Ingrese el apellido del contacto");
-//     const edad = prompt("Ingrese la edad del contacto");
-//     const mail = prompt("Ingrese el mail del contacto");
-//     const nacionalidad = prompt("Ingrese la nacionalidad del contacto");
-    
-//     const nuevoUsuario = new Usuario(nombre, apellido, edad, mail, nacionalidad);
-//     users.push(nuevoUsuario); // Agrega el objeto al array
-// }
-
-
-function agregarContacto() {
-    while (true) {
-        const nombre = prompt("Ingrese el nombre del contacto");
-        const apellido = prompt("Ingrese el apellido del contacto");
-        const edad = prompt("Ingrese la edad del contacto");
-        const mail = prompt("Ingrese el mail del contacto");
-        const nacionalidad = prompt("Ingrese la nacionalidad del contacto");
-
-        const nuevoUsuario = new Usuario(nombre, apellido, edad, mail, nacionalidad);
-        users.push(nuevoUsuario); // Agrega el objeto al array
-
-        const continuar = prompt("¿Desea agregar otro contacto? (si/no)").toLowerCase();
-        if (continuar !== "si") {
-            break; // Finaliza el bucle si el usuario no quiere agregar más contactos
-        }
-    }
-}
-
-function mostrarContactos(){
-    if (users == ""){
-        alert("La agenda esta vacia");
-    } else{
-        for (const user of users) {
-            console.log(`El usuario: ${user.nombre} ${user.apellido} con ${user.edad} años es ${user.nacionalidad} y su e-mail es: ${user.mail}`); 
-            alert(`El usuario: ${user.nombre} ${user.apellido} con ${user.edad} años es ${user.nacionalidad} y su e-mail es: ${user.mail}`); 
-        }
-    }
-}
-    
-// Constructor de objetos-----------
-
+// Constructor de objetos
 
 function Usuario (nombre, apellido, edad, mail, nacionalidad){
     this.nombre = nombre;
@@ -79,291 +36,155 @@ function Usuario (nombre, apellido, edad, mail, nacionalidad){
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    // // let option = null;
-    // // const numero = [];
-    // // const operador = [];
-    // let resultado;
-    // let ans;
-    // let i = 0;
-    // let cadena;
-    
-    // do{
-    //     cadena = prompt("Ingrese la operacion a evaluar, al terminar precione enter");
-    //     evaluar(cadena);
-    // } while (cadena.toLowerCase() != "salir");
-    
-    
-    // function evaluar(string) {
-    //     try {
-    //         const solucion = eval(string);
-    //         console.log("solucion: " + solucion);
-    //         alert("El resultado es: " + solucion);
-    //         return solucion;
-    //     } catch (error) {
-    //         console.log("Gracias por usar la Calculadora");
-    //         alert("Gracias por usar la Calculadora");
-    //         return null;
-    //     }
-    // }
-
-// do{
-//   elemento = prompt("Ingrese el " + (i+1) + " elemento");
-
-//     if (isNaN(elemento)) {
-//         operador.push(elemento);
-//     } else {
-//         numero.push(elemento);
-//     }
-
-
-//     console.log(resultado);
-//     i++;
-
-// } while (operador[-1] === "=");
-
-
-// function juntar() {
-//     const aux = [];
-
-//     for (let i = 0; i < numero.length; i++) {
-//         if (i > 0) {
-//             aux.push(operador[i - 1]);
-//         }
-//         aux.push(numero[i]);
-//         console.log(aux);
-//     }
-
-//     console.log(aux);
-//     resultado = aux.join("");
-//     console.log(resultado);
-//     return resultado
-// }
-
-
-// function juntar(){
-//     const aux = [];
-
-//     for ( let i = 0; i < numero.length; i++ ){
-//         aux.push(numero[i]+operador[i])
-//         console.log(aux);
-//     }
-//     console.log(aux);
-//     resultado = aux.join("");
-//     console.log(resultado);
-//     return resultado;
-// }
-
-// juntar();
-
-
-
-
-
-// option = prompt('¿Qué deseas hacer?\n\n1. Sumar.\n2. Restar.\n3. Multiplicar.\n4. Dividir.\n\nDe lo contrario, ingresa "salir" si quieres salir.');
-
-// while (option.toLowerCase() !== "salir") {
-//     switch (option) {
-//         case "1":
-//             ans = sumar();
-//             break;
-//         case "2":
-//             ans = restar();
-//             break;
-//         case "3":
-//             ans = multiplicar();
-//             break;
-//         case "4":
-//             ans = dividir();
-//             break;
-//         case "5":
-//             option = prompt('¿Qué deseas hacer con ' + ans + '?\n\n1. Sumar.\n2. Restar.\n3. Multiplicar.\n4. Dividir.\n\nDe lo contrario, ingresa "salir" si quieres salir.');
-//             while (option.toLowerCase() !== "salir") {
-//                 switch (option) {
-//                     case "1":
-//                         ans = sumarAns(ans);
-//                         break;
-//                     case "2":
-//                         ans = restarAns(ans);
-//                         break;
-//                     case "3":
-//                         ans = multiplicarAns(ans);
-//                         break;
-//                     case "4":
-//                         ans = dividirAns(ans);
-//                         break;
-//                     default:
-//                         alert("Opción no válida. Inténtalo de nuevo.");
-//                 }
-//                 option = prompt('¿Qué deseas hacer con ' + ans + '?\n\n1. Sumar.\n2. Restar.\n3. Multiplicar.\n4. Dividir.\n\nDe lo contrario, ingresa "salir" si quieres salir.');
-//             }
-//             break;
-//         default:
-//             alert("Opción no válida. Inténtalo de nuevo.");
-//     }
-//     option = prompt('¿Qué deseas hacer?\n\n1. Sumar.\n2. Restar.\n3. Multiplicar.\n4. Dividir.\n5. ANS.\n\nDe lo contrario, ingresa "salir" si quieres salir.');
-// }
-
-
-// function sumarAns(ans){
-//     num2 = pedirNumero("Ingrese el 2° sumando");
-//     resultado = ans + num2;
-//     if (!isNaN(resultado)) {
-//         console.log("La suma de " + ans + " + " + num2 + " = " + resultado);
-//         alert("La suma de " + ans + " + " + num2 + " = " + resultado);
-//         return resultado;
-//     } else {
-//         alert("Error!");
-//         return ans;
-//     }
-// }
-
-// function restarAns(ans){
-//     num2 = pedirNumero("Ingrese el sustraendo");
-//     resultado  = ans - num2;
-//     if (!isNaN(resultado)) {
-//         console.log("La diferencia de " + ans + " - " + num2 + " = " + resultado);
-//         alert("La diferencia de " + ans + " - " + num2 + " = " + resultado);
-//         return resultado;
-//     } else {
-//         alert("Error!");
-//         return ans;
-//     }
-// }
-
-// function dividirAns(ans){
-//     num2 = pedirNumero("Ingrese el dividendo");
-//     resultado = ans / num2;
-//     if (!isNaN(resultado)) {
-//         console.log("El cociente de " + ans + " / " + num2 + " = " + resultado);
-//         alert("El cociente de " + ans + " / " + num2 + " = " + resultado);
-//         return resultado;
-//     } else {
-//         alert("Error!");
-//         returnans;
-//     }
-// }
-
-// function multiplicarAns(ans){
-//     num2 = pedirNumero("Ingrese el multiplicador");
-//     resultado = ans * num2;
-//     if (!isNaN(resultado)) {
-//         console.log("El producto de " + ans + " * " + num2 + " = " + resultado);
-//         alert("El producto de " + ans + " * " + num2 + " = " + resultado);
-//         return resultado;
-//     } else {
-//         alert("Error!");
-//         return ans;
-//     }
-// }
-
-// function sumar(){
-//     let num1, num2;
-//     num1 = pedirNumero("Ingrese el 1° sumando");
-//     num2 = pedirNumero("Ingrese el 2° sumando");
-//     resultado = num1 + num2;
-//     if (!isNaN(resultado)) {
-//         console.log("La suma de " + num1 + " + " + num2 + " = " + resultado);
-//         alert("La suma de " + num1 + " + " + num2 + " = " + resultado);
-//         return resultado;
-//     } else {
-//         alert("Error!");
-//         return;
-//     }
-// }
-
-// function restar(){
-//     let num1, num2;
-//     num1 = pedirNumero("Ingrese el minuendo");
-//     num2 = pedirNumero("Ingrese el sustraendo");
-//     resultado  = num1 - num2;
-//     if (!isNaN(resultado)) {
-//         console.log("La diferencia de " + num1 + " - " + num2 + " = " + resultado);
-//         alert("La diferencia de " + num1 + " - " + num2 + " = " + resultado);
-//         return resultado;
-//     } else {
-//         alert("Error!");
-//         return;
-//     }
-// }
-
-// function dividir(){
-//     let num1, num2;
-//     num1 = pedirNumero("Ingrese el dividendo");
-//     num2 = pedirNumero("Ingrese el divisor");
-//     resultado = num1 / num2;
-//     if (!isNaN(resultado)) {
-//     console.log("El cociente de " + num1 + " / " + num2 + " = " + resultado);
-//     alert("El cociente de " + num1 + " / " + num2 + " = " + resultado);
-//     return resultado;
-//     } else {
-//         alert("Error!");
-//         return;
-//     }
-// }
-
-// function multiplicar(){
-//     let num1, num2;
-//     num1 = pedirNumero("Ingrese el multiplicando");
-//     num2 = pedirNumero("Ingrese el multiplicador");
-//     resultado = num1 * num2;
-//     if (!isNaN(resultado)) {
-//         console.log("El producto de " + num1 + " * " + num2 + " = " + resultado);
-//         alert("El producto de " + num1 + " * " + num2 + " = " + resultado);
-//         return resultado;
-//     } else {
-//         alert("Error!");
-//         return ans;
-//     }
-// }
+//Agrega objetos al array
+
+function agregarContacto() {
+    while (true) {
+        let edad;
+        let mail;
+        let nombre = prompt("Ingrese el nombre del contacto").trim();
+        let apellido = prompt("Ingrese el apellido del contacto").trim();         //Quita espacios al principio y al final
+        
+        while (isNaN(edad)) {
+            edad = parseInt(prompt("Ingrese la edad del contacto"));                // Comprobacion de la edad como número
+        }
+        
+        while (!mail || !mail.includes("@")) {                                      // Comprobacion del formato de correo electronico que incluya el @
+            mail = prompt("Ingrese el correo electronico del contacto (debe contener '@')");
+        }
+        
+        let nacionalidad = prompt("Ingrese la nacionalidad del contacto").trim();
+        
+        if (/^[a-z]/.test(nacionalidad)) {
+            nacionalidad = nacionalidad.charAt(0).toUpperCase() + nacionalidad.slice(1);
+        }
+        
+        if (/^[a-z]/.test(nombre)) {                                                //"/^/" hace referencia al primer elemento de la string "[a-z]" representa cualquier letra minuscula
+            nombre = nombre.charAt(0).toUpperCase() + nombre.slice(1);              // Convertir nombre a mayusculas si comienzan con minuscula
+        }
+        
+        if (/^[a-z]/.test(apellido)) {                                              //.test devuelve true o false segun /^[a-z]/ en la cadena entre parentesis
+            apellido = apellido.charAt(0).toUpperCase() + apellido.slice(1);        // Convertir apellido a mayusculas si comienzan con minuscula
+        }
+
+        const nuevoUsuario = new Usuario(nombre, apellido, edad, mail, nacionalidad);
+        users.push(nuevoUsuario);                                                   // Agrega el objeto al array
+        
+        const continuar = prompt("¿Desea agregar otro contacto? (si/no)").toLowerCase();
+        if (continuar !== "si") {
+            break;                                                                  // Finaliza el bucle si el usuario no quiere agregar mas contactos
+        }
+    }
+}
+
+
+//Mustra cada objeto del array
+
+function mostrarContactos(mostrarTodos = true) {
+    if (users.length === 0) {
+        alert("La agenda está vacía");
+    } else {
+        if (mostrarTodos) {
+            for (const user of users) {
+                console.log(`El usuario: ${user.nombre} ${user.apellido} con ${user.edad} años, su nacionalidad es ${user.nacionalidad} y su e-mail es: ${user.mail}`);
+                alert(`El usuario: ${user.nombre} ${user.apellido} con ${user.edad} años, su nacionalidad es ${user.nacionalidad} y su e-mail es: ${user.mail}`);
+            }
+        } else {
+            // Solo mostrar el contacto solicitado
+            const buscado = prompt("Ingresa el nombre o apellido del contacto a mostrar");
+            const encontrado = users.find(user => user.nombre === buscado || user.apellido === buscado);
+            
+            if (encontrado) {
+                console.log(`El usuario: ${encontrado.nombre} ${encontrado.apellido} con ${encontrado.edad} años, su nacionalidad es ${encontrado.nacionalidad} y su e-mail es: ${encontrado.mail}`);
+                alert(`El usuario: ${encontrado.nombre} ${encontrado.apellido} con ${encontrado.edad} años, su nacionalidad es ${encontrado.nacionalidad} y su e-mail es: ${encontrado.mail}`);
+            } else {
+                alert("Contacto no encontrado");
+            }
+        }
+    }
+}
+
+
+//Edita un objeto dentro del array
+
+function editarContacto() {
+    const mostrarTodos = confirm("¿Quieres mostrar todos los contactos antes de editar?");
+    mostrarContactos(mostrarTodos);
+
+    let buscado = prompt("Ingrese el nombre o apellido del contacto que desea editar");
+    let contactoEncontrado = null;
+
+    if (buscado.trim() === "") {
+        alert("Nombre o apellido no válido.");
+        return;
+    }
+
+    for (const user of users) {
+        if (user.nombre.toLowerCase() === buscado.toLowerCase() || user.apellido.toLowerCase() === buscado.toLowerCase()) {
+            contactoEncontrado = user;
+            console.log(`El usuario: ${contactoEncontrado.nombre} ${contactoEncontrado.apellido} con ${contactoEncontrado.edad} años, su nacionalidad es ${contactoEncontrado.nacionalidad} y su e-mail es: ${contactoEncontrado.mail}`);
+            alert(`El usuario: ${contactoEncontrado.nombre} ${contactoEncontrado.apellido} con ${contactoEncontrado.edad} años, su nacionalidad es ${contactoEncontrado.nacionalidad} y su e-mail es: ${contactoEncontrado.mail}`);
+            const edit = prompt("Ingrese: nombre, apellido, edad, mail, nacionalidad o todos. Segun el campo que quiera editar");
+            switch (edit.toLowerCase().trim()){
+                case "nombre":
+                    contactoEncontrado.nombre = prompt("Ingrese el nuevo nombre del contacto").trim();
+                    if (/^[a-z]/.test(contactoEncontrado.nombre)) {
+                        contactoEncontrado.nombre = contactoEncontrado.nombre.charAt(0).toUpperCase() + contactoEncontrado.nombre.slice(1);
+                    }
+                    break;
+                case "apellido":
+                    contactoEncontrado.apellido = prompt("Ingrese el nuevo apellido del contacto").trim();
+                    if (/^[a-z]/.test(contactoEncontrado.apellido)) {
+                        contactoEncontrado.apellido = contactoEncontrado.apellido.charAt(0).toUpperCase() + contactoEncontrado.apellido.slice(1);
+                    }
+                    break;
+                case "edad":
+                    do{
+                        contactoEncontrado.edad = parseInt(prompt("Ingrese la nueva edad del contacto"));
+                    } while (isNaN(contactoEncontrado.edad))
+                    break;
+                case "mail":
+                    contactoEncontrado.mail = null;
+                    while (!mail || !mail.includes("@")) {
+                        contactoEncontrado.mail = prompt("Ingrese el nuevo correo electronico del contacto (debe contener '@')").trim();
+                    }
+                    break;
+                case "nacionalidad":
+                    contactoEncontrado.nacionalidad = prompt("Ingrese la nueva nacionalidad del contacto").trim();
+                    if (/^[a-z]/.test(contactoEncontrado.nacionalidad)) {
+                        contactoEncontrado.nacionalidad = contactoEncontrado.nacionalidad.charAt(0).toUpperCase() + contactoEncontrado.nacionalidad.slice(1);
+                    }
+                    break;
+                case "todos":
+                    contactoEncontrado.nombre = prompt("Ingrese el nuevo nombre del contacto").trim();
+                    if (/^[a-z]/.test(contactoEncontrado.nombre)) {
+                        contactoEncontrado.nombre = contactoEncontrado.nombre.charAt(0).toUpperCase() + contactoEncontrado.nombre.slice(1);
+                    }
+                    contactoEncontrado.apellido = prompt("Ingrese el nuevo apellido del contacto").trim();
+                    if (/^[a-z]/.test(contactoEncontrado.apellido)) {
+                        contactoEncontrado.apellido = contactoEncontrado.apellido.charAt(0).toUpperCase() + contactoEncontrado.apellido.slice(1);
+                    }
+                    do{
+                        contactoEncontrado.edad = parseInt(prompt("Ingrese la nueva edad del contacto"));
+                    } while (isNaN(contactoEncontrado.edad))
+                    contactoEncontrado.mail = null;
+                    while (!mail || !mail.includes("@")) {
+                        contactoEncontrado.mail = prompt("Ingrese el nuevo correo electronico del contacto (debe contener '@')").trim();
+                    }
+                    contactoEncontrado.nacionalidad = prompt("Ingrese la nueva nacionalidad del contacto").trim();
+                    if (/^[a-z]/.test(contactoEncontrado.nacionalidad)) {
+                        contactoEncontrado.nacionalidad = contactoEncontrado.nacionalidad.charAt(0).toUpperCase() + contactoEncontrado.nacionalidad.slice(1);
+                    }
+                    break;
+                default:
+                    alert("Opcion incorrecta. Gracias, vuelva pronto.");
+                    break;
+                break;
+            }
+        }
+        if (!contactoEncontrado) {
+            alert("Contacto no encontrado.");
+            return;
+        }
+    }
+}
